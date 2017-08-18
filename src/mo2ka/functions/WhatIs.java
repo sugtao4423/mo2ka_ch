5380dab975c082ea1d01d80f12a72cdbf7b743a8
tao <sugtao4423@gmail.com>
2017-08-19 02:01:51 +0900

    what is?にURLをつけるの忘れてた

--- src/mo2ka/functions/WhatIs.java
+++ src/mo2ka/functions/WhatIs.java
@@ -1,5 +1,7 @@
 package mo2ka.functions;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -10,18 +12,18 @@ public class WhatIs{
 
 	public WhatIs(Status status, String what){
 		try{
-			ResultSet r = Momoka.wikiStmt.executeQuery("select abstract from wikipedia where title = '" + what + "'");
+			ResultSet r = Momoka.wikiStmt.executeQuery("select * from wikipedia where title = '" + what + "'");
 			if(r.next()){
-				tweet(status, r.getString(1));
+				tweet(status, r.getString(1), r.getString(2));
 				r.close();
 				return;
 			}
 
-			r = Momoka.wikiStmt.executeQuery("select abstract from wikipedia where title like '" + what + "'");
+			r = Momoka.wikiStmt.executeQuery("select * from wikipedia where title like '" + what + "'");
 			if(r.next()){
-				tweet(status, r.getString(1));
+				tweet(status, r.getString(1), r.getString(2));
 			}else{
-				tweet(status, "単語が見つかりませんでした");
+				tweet(status, null, "単語が見つかりませんでした");
 			}
 			r.close();
 		}catch(SQLException e){
@@ -29,11 +31,18 @@ public class WhatIs{
 		}
 	}
 
-	private void tweet(Status status, String abs){
+	private void tweet(Status status, String title, String abs){
 		int absMaxSize = 140 - status.getUser().getScreenName().length() - 38;
 		if(abs.length() > absMaxSize)
 			abs = abs.substring(0, absMaxSize - 3) + "...";
-		new Tweet("@" + status.getUser().getScreenName() + " " + abs, status.getId());
+		String url = "";
+		try{
+			if(title != null)
+				url = "https://ja.wikipedia.org/wiki/" + URLEncoder.encode(title, "UTF-8");
+		}catch(UnsupportedEncodingException e){
+			System.err.println("Could not encode url\nMessage: " + e.getMessage());
+		}
+		new Tweet("@" + status.getUser().getScreenName() + " " + abs + " " + url, status.getId());
 	}
 
 }
